ca7f3af835667ad288201850e3afcfc4b8c6915c
Shuheb Ahmed <shuhebahmed@Shuhebs-MacBook-Pro.local>
2019-09-04 15:31:03 +0100

    added more features

--- scripts/main.js
+++ scripts/main.js
@@ -2,38 +2,50 @@ var columnDefs = [
     {
         field: "from",
         checkboxSelection: true,
-        width: 240,
-        cellStyle: {'font-weight': 'bold'},
+        width: 400,
         // cellRenderer: function (params) {
-        //     let icon = `<span><img src="https://www.gstatic.com/images/icons/material/system/1x/star_border_black_20dp.png" style="padding-right: 40px;"/></span>`
+        //     let icon = `<span><img src="https://www.gstatic.com/images/icons/material/system/1x/star_border_black_20dp.png" style="padding-right: 20px; position: relative; top: 5px""/></span>`
         //     return icon + params.value;
         // }
     },
-    {field: "subject", width: 1092, cellRenderer: 'hoverCellRenderer', cellStyle: {'font-weight': 'bold'}},
+    {field: "subject", width: 1092, cellRenderer: 'hoverCellRenderer',},
 ];
 
-var rowData = [
-    {from: 'xxxx', subject: 'xxxxxxxxxxxx', emailData: 'kjkljmkofo jgoidjf goidfjgiodfjgoi', read: false},
-    {from: 'yyyy', subject: 'yyyyyyyyyyyy', emailData: 'knsfkjdsnfkanjd', read: false},
-    {from: 'wwww', subject: 'wwwwwwwwwwww', emailData: 'vfjkndlvjksfbdiln lsdknfsdfoiudshufidsnfklj sdnklsdb fisdkflsd', read: false},
-    {from: 'zzzz', subject: 'zzzzzzzzzzzz', emailData: 'rewoiurheoiuthrouit hoiutrehg ieufhiouewghroiqwu das', read: true}
-];
 var gridOptions = {
     columnDefs: columnDefs,
-    rowData: rowData,
     rowHeight: 40,
     defaultColDef: {
         sortable: true,
         resizable: true,
     },
-    rowSelection: 'single',
+    rowSelection: 'multiple',
+    suppressRowClickSelection: true,
+    deltaRowDataMode: true,
+    pagination: true,
+    paginationAutoPageSize: true,
+    getRowNodeId: function (data) {
+        return data.id;
+    },
     components: {
         hoverCellRenderer: HoverCellRenderer,
     },
+    rowClassRules: {
+        'email-read': function (params) {
+            return !params.data.read;
+        },
+        'row-selected': function (params) {
+            return params.node.selected;
+        }
+    },
     onCellMouseOver: onCellMouseOver,
     onCellMouseOut: onCellMouseOut,
-    onRowSelected: onRowSelected,
+    onRowClicked: onRowClicked,
+    onFirstDataRendered: () => gridOptions.api.sizeColumnsToFit()
 };
+
+function processData(data) {
+    gridOptions.api.setRowData(data);
+}
 
 function onCellMouseOver(event) {
     //find which cell you are hoverin in, show button
@@ -55,14 +67,42 @@ function onCellMouseOut(event) {
     renderer.hideButton();
 }
 
-function onRowSelected(event) {
+function onRowClicked(event) {
+
+    if (event.event.target.tagName === "IMG") {
+        event.event.stopPropagation();
+        return
+    }
+
+
+    console.log('Row Clicked', event);
     let from = event.data.from;
     let subject = event.data.subject;
     let emailData = event.data.emailData;
-    event.data.read = true;
-    document.getElementById('emailFrom').innerText = 'From: '+from;
-    document.getElementById('emailSubject').innerText = 'RE: '+subject;
+
+    let newData = {
+        ...event.data,
+        read: true
+    };
+
+    event.node.setData(newData);
+    document.getElementById('emailFrom').innerText = 'From: ' + from;
+    document.getElementById('emailSubject').innerText = 'RE: ' + subject;
     document.getElementById('emailData').innerText = emailData;
+}
+
+function onSelectAll(event) {
+    if (!event.selected) {
+        event.selected = !event.selected;
+        event.src = "https://www.gstatic.com/images/icons/material/system/1x/check_box_black_20dp.png";
+        gridOptions.api.selectAll();
+    } else {
+        event.selected = !event.selected;
+        event.src = "https://www.gstatic.com/images/icons/material/system/1x/check_box_outline_blank_black_20dp.png";
+        gridOptions.api.deselectAll();
+    }
+    url('https://www.gstatic.com/images/icons/material/system/1x/check_box_black_20dp.png')
+    url('https://www.gstatic.com/images/icons/material/system/1x/check_box_outline_blank_black_20dp.png')
 }
 
 function HoverCellRenderer() {
@@ -71,18 +111,57 @@ function HoverCellRenderer() {
 HoverCellRenderer.prototype.init = function (params) {
 
     this.eGui = document.createElement('div');
-    this.eGui.innerHTML =
-        ` ${params.value} <span class = "btn-simple" style="display: none; float:right;">
-        <span class="row-icon-hover" style="margin:0 10px;"><img src="https://www.gstatic.com/images/icons/material/system/1x/archive_black_20dp.png"></span>
-        <span class="row-icon-hover" style="margin:0 10px;"><img src="https://www.gstatic.com/images/icons/material/system/1x/delete_black_20dp.png"></span>
-        <span class="row-icon-hover" style="margin:0 10px;" ><img src="https://www.gstatic.com/images/icons/material/system/1x/drafts_black_20dp.png"></span>
-        <span class="row-icon-hover" style="margin:0 10px;"><img src="https://www.gstatic.com/images/icons/material/system/1x/watch_later_black_20dp.png"></span>`;
-    this.eButton = this.eGui.querySelector('.btn-simple');
+    this.eGui.innerHTML = `
+<div style="display: flex; flex-direction: row">
+     <div style="overflow: hidden;"><span style="text-overflow: ellipsis; display: inline-block; overflow: hidden; font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif ;font-size: .875rem; letter-spacing: .2px">${params.value} - <span style="font-weight: normal; color: #5f6368;">${params.node.data.emailData}</span></span></div>
+     <div class = "btns" style="width: 100%; text-align: right; display: none;">
+<!--        <span style="display: none; float:right;">-->
+        <span class="btn-archive row-icon-hover" style="margin:0 10px;"><img src="https://www.gstatic.com/images/icons/material/system/1x/archive_black_20dp.png"></span>
+        <span class="btn-delete row-icon-hover" style="margin:0 10px;"><img src="https://www.gstatic.com/images/icons/material/system/1x/delete_black_20dp.png"></span>
+        <span class="btn-mark row-icon-hover" style="margin:0 10px;" ><img src="https://www.gstatic.com/images/icons/material/system/1x/drafts_black_20dp.png"></span>
+        <span class="btn-snooze row-icon-hover" style="margin:0 10px;"><img src="https://www.gstatic.com/images/icons/material/system/1x/watch_later_black_20dp.png"></span></span>
+     </div>
+</div>`;
+    this.eArchiveButton = this.eGui.querySelector('.btn-archive');
+    this.eDeleteButton = this.eGui.querySelector('.btn-delete');
+    this.eMarkButton = this.eGui.querySelector('.btn-mark');
+    this.eSnoozeButton = this.eGui.querySelector('.btn-snooze');
 
-    this.eventListener = function () {
-        console.log('*** Insert button logic ***');
-    };
-    this.eButton.addEventListener('click', this.eventListener);
+    this.eArchiveButton.addEventListener('click', function (event) {
+        console.log('*** Archive Button clicked ***');
+    });
+
+    this.eDeleteButton.addEventListener('click', function (event) {
+        console.log('*** Delete Button clicked ***', params.node);
+
+        console.log('params', params);
+
+        // let newData = [];
+        // params.api.forEachNode (node=>{
+        //     if (node === params.node) return;
+        //     newData.push(node.data)
+        // })
+
+        params.api.updateRowData({remove: [params.node]});
+
+
+    });
+
+    this.eMarkButton.addEventListener('click', function (event) {
+        console.log('*** Mark Button clicked ***');
+        console.log('params', params);
+
+        let newData = {
+            ...params.data,
+            read: false
+        };
+
+        params.node.setData(newData);
+    });
+
+    this.eSnoozeButton.addEventListener('click', function (event) {
+        console.log('*** Snooze Button clicked ***');
+    });
 };
 
 HoverCellRenderer.prototype.getGui = function () {
@@ -90,7 +169,7 @@ HoverCellRenderer.prototype.getGui = function () {
 };
 
 HoverCellRenderer.prototype.showButton = function () {
-    this.spanEl = this.eGui.querySelector('span');
+    this.spanEl = this.eGui.querySelector('.btns');
     this.spanEl.style.display = 'inline-block';
 };
 
@@ -104,5 +183,5 @@ document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 
-
+    fetch('data.json').then(result => result.json().then(data => processData(data.map((item, i) => ({id: i, ...item})))));
 });
