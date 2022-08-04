16f14c5c8f83db74a9c116bbf80000ad63d36923
shuheb <37620997+shuheb@users.noreply.github.com>
2022-08-04 15:56:19 +0100

    added action renderer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,80 @@
 import { Grid } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+import '../styles.css';
 import CustomCellRenderer from './customCellRenderer';
-debugger;
+// import data from '../data.json';
+
+const fetchData = async () => {
+    const response = await fetch('https://dev.to/feed/ag-grid');
+    const text = await response.text();
+    const data = await new window.DOMParser().parseFromString(text, 'text/xml');
+    const items = [];
+
+    data.querySelectorAll('item').forEach((el, index) => {
+        items.push({
+            id:index,
+            title: el.querySelector('title').textContent,
+            creator: el.querySelector('creator').textContent,
+            pubDate: new Date(el.querySelector('pubDate').textContent),
+            link: el.querySelector('link').textContent,
+            guid: el.querySelector('guid').textContent,
+            description: el.querySelector('description').textContent,
+            category: el.querySelector('category').textContent,
+            read: false
+        });
+    });
+
+    gridOptions.api.setRowData(items);
+};
+
 const columnDefs = [
-    { field: "make", cellRenderer: CustomCellRenderer },
-    { field: "model" },
-    { field: "price" }
+    {
+        headerName:'Fretewtrewtertom',
+        colId: 'creator',
+        field: 'creator',
+        maxWidth:250,
+        checkboxSelection: true
+    },
+    {
+        headerName:'Subject',
+        colId: 'title',
+        field: 'title',
+        cellRenderer: CustomCellRenderer
+    },
+    // { colId: 'pubDate', field: 'pubDate' },
+    // { colId: 'link', field: 'link' },
+    // { colId: 'guid', field: 'guid' },
+    // { colId: 'description', field: 'description' },
+    // { colId: 'category', field: 'category' }
 ];
 
-const rowData = [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxster", price: 72000 }
-];
+const rowData = [];
+const defaultColDef = {
+    flex: 1,
+}
+
+function onGridReady() {
+    fetchData();
+}
 
 const gridOptions = {
     rowData,
-    columnDefs
+    columnDefs,
+    defaultColDef,
+    rowHeight: 50,
+    getRowId: params => {
+        return params.data.id;
+    },
+    onRowClicked: params => {
+        params.node.setData({ ...params.data, read: !params.data.read })
+    },
+    getRowStyle: ({ data }) => {
+        return data.read ?
+            { fontWeight: 'normal', backgroundColor: 'rgba(242, 245, 245, 0.8)' } :
+            { fontWeight: 'bold', backgroundColor: 'rgb(255, 255, 255)' }
+    },
+    onGridReady: onGridReady
 };
 
 const eGridDiv = document.getElementById('myGrid');
